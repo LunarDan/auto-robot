069ccbbc8b8247be828535b832978de7dad20144
LunarDan <wujunhao2026@gmail.com>
2026-03-22 10:38:55 +0800

    fix: create pictures directory before saving

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const request = require("request");
-const path = require("path");
 const fs = require("fs");
-const id = (~~(Math.random() * 100000)).toString(); // 获取小于10w的数字
+const path = require("path");
+const request = require("request");
+
+const id = (~~(Math.random() * 100000)).toString();
 const url = `https://robohash.org/${id}`;
 const dirPath = path.resolve(__dirname, "pictures");
-// 这一步的处理，因为github获取的时间时区是美国时区，所以获取到的时间格式是6/16/2023，我们可以先split后将年份放到数组第一位，这样就是需要的文件名格式了
-const dateArr = new Date().toLocaleDateString().split("/"); // 本地调试时用.toLocaleDateString("en")
-dateArr.unshift(dateArr.pop());
-const date = dateArr.join("-");
-request(url).pipe(fs.createWriteStream(`${dirPath}/${date}.png`));
+const now = new Date();
+const date = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+const outputPath = path.join(dirPath, `${date}.png`);
+
+fs.mkdirSync(dirPath, { recursive: true });
+
+const writeStream = fs.createWriteStream(outputPath);
+
+writeStream.on("error", (error) => {
+  console.error("Failed to write robot image:", error);
+  process.exitCode = 1;
+});
+
+request(url)
+  .on("error", (error) => {
+    console.error("Failed to download robot image:", error);
+    process.exitCode = 1;
+  })
+  .pipe(writeStream);
